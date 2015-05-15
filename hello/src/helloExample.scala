68281672e2e215d646b2ff14b6eb7f2a1450131a
fatbeans <xindianden@gmail.com>
2015-05-15 16:15:20 +0800

    learn

---
+++ hello/src/helloExample.scala
@@ -0,0 +1,8 @@
+/**
+ * Created by xinxin on 2015/5/15.
+ */
+object helloExample {
+  def main(args: Array[String]) {
+    print(123)
+  }
+}
---
+++ src/HelloScala.scala
@@ -0,0 +1,9 @@
+/**
+ * Created by xinxin on 2015/5/15.
+ */
+class HelloScala {
+
+  def main(args: Array[String]) {
+   println("123123123")
+  }
+}
